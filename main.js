a89a36df0adf6c00388b76763c68d36b4bbb95f1
dp25032004 <115575061+dp25032004@users.noreply.github.com>
2023-01-24 19:45:07 +0000

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var menu = document.querySelector("nav ul");
-var menuBar = document.querySelector("nav .menu-icon");
-var iconMenu = document.querySelector("nav .menu-icon img");
-
-menuBar.addEventListener("click", function(){
-    menu.classList.toggle("active")
-})
